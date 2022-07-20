9f7f55f313f0a6a199380d822219622f59399d3c
Asmita Dulla <saiasmitadulla@gmail.com>
2022-07-20 14:38:20 -0700

    Initial Commit

---
+++ FINAL PROJECT/FINAL_PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINAL_PROJECTApp.swift
+//  FINAL PROJECT
+//
+//  Created by Asmita Dulla on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct FINAL_PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
